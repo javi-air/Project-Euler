0687585703fe33ba6ee2235e36f63a6c59395103
javi-air <je8@williams.edu>
2016-10-25 23:51:36 -0400

    Update Problem 1.py

--- Solved/Problem 1.py
+++ Solved/Problem 1.py
@@ -3,4 +3,4 @@ def multiples_sum(number1, number2, max):
   This function will find the sum of all the natural numbers below parameter "max"
   that are multiples of number1 or number2
   '''
-  return sum([x for x in range(max) if x%number1 == 0 or x%number2 == 0])
+  return sum([x for x in range(max) if x % number1 == 0 or x % number2 == 0])
